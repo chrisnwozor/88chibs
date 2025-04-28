2295d13087a3c12e0b8d47a0259275c69bfa148f
Nwozor Christopher <christophernwozor@gmail.com>
2025-04-28 15:02:09 +0100

    client

--- sanity/lib/client.js
+++ sanity/lib/client.js
@@ -6,6 +6,11 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: false,
+  useCdn: true,
   token: process.env.NEXT_PUBLIC_FRONTEND_ORDER_TOKEN,
+  stega: {
+    studioUrl: process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/studio`
+      : `${process.env.NEXT_PUBLIC_BASE_URL}/studio`,
+  },
 });
