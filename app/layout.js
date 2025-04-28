0b32d6c656f21a9abb51ed658de902789f4efa9d
Nwozor Christopher <christophernwozor@gmail.com>
2025-04-28 14:22:12 +0100

    added a variable

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      frontendApi={process.env.NEXT_PUBLIC_CLERK_FRONTEND_API}
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <html lang="en" className="scroll-smooth" suppressHydrationWarning={true}>
         <body className={`${anton.className} antialiased bg-primary`}>
           {children}
